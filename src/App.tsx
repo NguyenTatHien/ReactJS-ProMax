85b141faa4f2183ccc4f49bd825d451ad34c478c
NguyenTatHien <tathien2003@gmail.com>
2025-03-01 10:25:59 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const MyFirstComponent = () => {
+
+  return (
+    <div>
+      my first component
+    </div>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <MyFirstComponent></MyFirstComponent>
+
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
